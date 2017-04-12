f70ea47b7dc8e5e840023fe396a01c1e7a99d6c2
Alexander Davis <akonidavis@gmail.com>
2017-04-11 16:51:48 -1000

    planet movie titles

--- js/app.js
+++ js/app.js
@@ -13,31 +13,65 @@ reqButton.addEventListener('click', function(event){
 
 	const oneReq = new XMLHttpRequest();
 
-
-	oneReq.addEventListener('load', reqPerson);
-	oneReq.open('GET', 'http://swapi.co/api/' + resourceType.value + '/'+ resourceId.value + '/');
-	oneReq.send();
-	function reqPerson() {
-		const requestData = JSON.parse(this.responseText);
-		const name = requestData.name;
-		one.innerHTML = 'Name: ' + name;		
-		const gender = requestData.gender;
-		two.innerHTML = `Gender: ${gender}`;
-		const twoReq = new XMLHttpRequest();
-		twoReq.open('GET', gender);
-		twoReq.addEventListener('load', reqPerson);
-		twoReq.open('GET', gender);
-		// request the species
-    	const speciesReq = new XMLHttpRequest();
-    	speciesReq.addEventListener('load', speciesRequire);
-    	speciesReq.open('GET', requestData.species);
-    	speciesReq.send();
-
-    	function speciesRequire() {
-      	const requestSpecies = JSON.parse(this.responseText);
-      	three.innerHTML = requestSpecies.name;
-
+	if(resourceType.value === "people"){
+		oneReq.addEventListener('load', reqPerson);
+		oneReq.open('GET', 'http://swapi.co/api/' + resourceType.value + '/'+ resourceId.value + '/');
+		oneReq.send();
+		function reqPerson() {
+			const requestData = JSON.parse(this.responseText);
+			const name = requestData.name;
+			one.innerHTML = 'Name: ' + name;		
+			const gender = requestData.gender;
+			two.innerHTML = `Gender: ${gender}`;
+			const twoReq = new XMLHttpRequest();
+			twoReq.open('GET', gender);
+			twoReq.addEventListener('load', reqPerson);
+    		const speciesReq = new XMLHttpRequest();
+    		speciesReq.addEventListener('load', speciesRequire);
+    		speciesReq.open('GET', requestData.species);
+    		speciesReq.send();
+    		function speciesRequire() {
+      		const requestSpecies = JSON.parse(this.responseText);
+      		three.innerHTML = requestSpecies.name;
     	}
 	}
 	
-});	
+		
+	}
+	if(resourceType.value === "planets"){
+		oneReq.addEventListener('load', reqPlanet);
+		oneReq.open('GET', 'http://swapi.co/api/' + resourceType.value + '/'+ resourceId.value + '/');
+		oneReq.send();
+
+		function reqPlanet() {
+			const requestData = JSON.parse(this.responseText);
+			const name = requestData.name;
+			one.innerHTML = 'Name: ' + name;		
+			const terrain = requestData.terrain;
+			two.innerHTML = `terrain: ${terrain}`;
+			const population = requestData.population;
+			three.innerHTML = `population: ${population}`;
+			const planetFilms = requestData.films;
+			//four.innerHTML = planetFilms;
+
+
+			for(let i =0; i<requestData.films.length; i++){
+				const reqFilm = new XMLHttpRequest();
+				reqFilm.addEventListener('load', filmRequire);
+				reqFilm.open('GET', requestData.films[i]);
+				reqFilm.send();
+
+			}
+				function filmRequire(){
+					const filmData = JSON.parse(this.responseText);
+    				let newList = document.createElement('li');
+    				const planetName = filmData.title;
+    				newList.innerHTML = planetName;
+    				four.appendChild(newList);
+    				console.log(filmData.title);
+				}
+		}
+	}
+}
+
+);	
